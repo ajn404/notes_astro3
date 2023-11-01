a81389a5bec4fb2283edcb200f7c9829ec79cb9e
ajn404 <nhy1423238345@163.com>
2023-11-01 11:30:09 +0800

    add config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,37 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import lit from '@astrojs/lit';
 import vue from '@astrojs/vue';
-import tailwind from "@astrojs/tailwind";
+// import vue  from "@vitejs/plugin-vue";
+import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./plugin/remark-reading-time.mjs";
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-css-only'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import path from 'path';
+import { fileURLToPath } from 'url'
 
+const __filenameNew = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filenameNew)
+console.log('dir name', __dirname);
 // https://astro.build/config
 export default defineConfig({
+    server: {
+        port: 3333,
+        open: true,
+    },
+    build: {
+        assets: "static",
+        inlineStylesheets: "always",
+    },
     site: 'https://example.com',
     integrations: [
         mdx(),
@@ -25,5 +48,60 @@ export default defineConfig({
                 applyBaseStyles: false,
             },
         })
-    ]
+    ],
+    markdown: {
+        remarkPlugins: [
+            [
+                remarkToc,
+                {
+                    heading: "目录",
+                },
+            ],
+            [
+                remarkCollapse,
+                {
+                    test: "脚本",
+                    summary: str => "展开 " + str,
+                },
+            ],
+            remarkMath,
+            remarkReadingTime,
+        ],
+        rehypePlugins: [rehypeKatex, rehypeAutolinkHeadings],
+        shikiConfig: {
+            theme: "one-dark-pro",
+            wrap: true,
+        },
+        extendDefaultPlugins: true,
+    },
+    vite: {
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
+        ssr: {
+            noExternal: [
+                "@notes/editor", "@shoelace-style/shoelace"
+            ]
+        },
+        build: {
+            rollupOptions: {
+                plugins: [
+                    // resolve(),
+                    // commonjs(),
+                    copy({
+                        copyOnce: true,
+                        targets: [
+                            {
+                                src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
+                                dest: path.resolve(__dirname, 'dist/shoelace')
+                            }
+                        ]
+                    }),
+                    css({
+                        output: 'bundle.css'
+                    }),
+                ]
+            }
+        }
+    },
 });
