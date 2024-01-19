4867fbabce0bc18c3888442e83bb5cd7f1c89ea3
ajn404 <nhy1423238345@163.com>
2024-01-19 17:37:51 +0800

    optimize code

--- src/components/react/p5/index.tsx
+++ src/components/react/p5/index.tsx
@@ -27,7 +27,6 @@ interface Props {
 const Basic = memo(({ sketch, showControls }: Props) => {
   const container = useRef(null);
   let p: p5;
-  let stopping = false;
 
   const start = async () => {
     if (container.current) {
@@ -37,34 +36,22 @@ const Basic = memo(({ sketch, showControls }: Props) => {
       container.current.style.minHeight = p.height + "px";
     }
   };
-  const remove = () => {
-    p && p.remove();
-  };
-  const stop = () => {
-    stopping = true;
-    p.isLooping() && p.noLoop();
-  };
-  const begin = () => {
-    stopping = false;
-    !p.isLooping() && p.loop();
-  };
-
+  const remove = () => p && p.remove();
+  const stop = () => p.isLooping() && p.noLoop();
+  const begin = () => !p.isLooping() && p.loop();
   const init = async () => {
     p && remove();
     await start();
   };
 
   useEffect(() => {
-    const dd = async () => {
+    (async () => {
       await init();
       const obs = new IntersectionObserver(tar => {
         !tar[0].isIntersecting && stop();
       });
       obs.observe(container.current);
-    };
-
-    dd();
-
+    })();
     return remove;
   });
 
@@ -87,7 +74,7 @@ const Basic = memo(({ sketch, showControls }: Props) => {
           <button
             type="button"
             onClick={begin}
-            className="inline-block text-skin-inverted rounded bg-skin-inverted px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
+            className="inline-block text-skin-inverted rounded bg-skin-green  bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
           >
             begin
           </button>
