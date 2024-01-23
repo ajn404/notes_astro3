5dedba0c8ac596ace291e1c5767bb015a8211a4c
ajn404 <nhy1423238345@163.com>
2024-01-23 16:37:22 +0800

    annotation

--- src/components/react/little/roughAnnotation.tsx
+++ src/components/react/little/roughAnnotation.tsx
@@ -1,17 +1,24 @@
 import { useEffect } from "react";
-import { annotate } from "rough-notation";
+import { annotate, annotationGroup } from "rough-notation";
 // Or using unpkg
 // import { annotate } from 'https://unpkg.com/rough-notation?module';
 
 export default () => {
   useEffect(() => {
-    const e: HTMLElement = document.querySelector("#myElement");
-    const annotation = annotate(e, { type: "underline" });
-    annotation.show();
+    const a1 = annotate(document.querySelector("#e1"), { type: "underline" });
+    const a2 = annotate(document.querySelector("#e2"), { type: "box" });
+    const a3 = annotate(document.querySelector("#e3"), { type: "circle" });
+
+    const ag = annotationGroup([a3, a1, a2]);
+    ag.show();
   });
   return (
     <>
-      <div id="myElement">添加annotation</div>
+      <div className="flex text-skin-base gap-8 ">
+        <span id="e1">e1</span>
+        <span id="e2">e2</span>
+        <span id="e3">e3</span>
+      </div>
     </>
   );
 };
