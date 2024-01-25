4cce0f46f90a853a8587bbbca3a32fedd61f9cf8
ajn404 <nhy1423238345@163.com>
2024-01-25 18:31:32 +0800

    success add button shadcn

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,14 +10,16 @@ function withOpacity(variableName) {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     important: true,
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-        "./node_modules/tw-elements/dist/js/**/*.js",],
+    content:
+        [
+            "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+            "./node_modules/tw-elements/dist/js/**/*.js"
+        ],
+    prefix: "",
     theme: {
-
         screens: {
             sm: "640px",
         },
-
         textColor: {
             skin: {
                 base: withOpacity("--color-text-base"),
@@ -74,7 +76,75 @@ module.exports = {
         fontFamily: {
             mono: ["IBM Plex Mono", "monospace"],
         },
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            colors: {
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
     },
-    plugins: [require("@tailwindcss/typography"), require("tw-elements/dist/plugin.cjs")],
+
+    plugins: [
+        require("@tailwindcss/typography"),
+        //下面两个插件好像有点冲突
+        require("tw-elements/dist/plugin.cjs"),
+        require("tailwindcss-animate")],
     darkMode: "class"
 };
