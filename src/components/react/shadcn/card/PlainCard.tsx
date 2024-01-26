f3701a91c4a141ab04530790d70eb8b4b32e050f
ajn404 <nhy1423238345@163.com>
2024-01-26 17:58:42 +0800

    change style

--- src/components/react/shadcn/card/PlainCard.tsx
+++ src/components/react/shadcn/card/PlainCard.tsx
@@ -6,6 +6,8 @@ import {
   CardTitle,
 } from "@shadcn/ui/card";
 
+import { buttonVariants } from "@shadcn/ui/button";
+
 import {
   forwardRef,
   type AnchorHTMLAttributes,
@@ -15,19 +17,23 @@ import {
 export default forwardRef<
   HTMLAnchorElement,
   React.HTMLAttributes<HTMLAnchorElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <Card>
     <CardHeader>
       <CardTitle>{props.title}</CardTitle>
     </CardHeader>
     <CardContent>
       <slot name="des" />
+      {children}
     </CardContent>
     <CardFooter>
       <a
-        href={props.title ? "#" : props.title}
-        className={`group inline-block ${className}`}
+        href={props.title ? "/notes_astro3/fantastic/" + props.title : "#"}
+        className={`group inline-block ${className} ${buttonVariants({
+          variant: "outline",
+        })}`}
         aria-label={props.title}
+        target="_blank"
         title={props.title}
       >
         跳转
